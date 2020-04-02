c328247d4575cd843af81a719170f009548f2d4a
Kamizuru <62087721+KamizuruGames@users.noreply.github.com>
2020-04-02 15:52:12 -0300

    fix tradução
    fix tradução

--- skillsList.js
+++ skillsList.js
@@ -1,41 +1,41 @@
 module.exports = {
 	BossActionsTips : {
-		//101: {msg: 'Hammer (block) -> 270 -> Knockback'},
-		//103: {msg: 'Front (Dodge)'},//103 104
-		//105: {msg: '270 sweep (block) -> Knockback'},
+		//101: {msg: 'MARTELO (BLOQUEIO) -> 270 -> BATIDA ATRAS'},
+		//103: {msg: 'FRENTE (ESQUIVA)'},//103 104
+		//105: {msg: '270 VARREDURA (BLOQUEIO) -> BATIDA ATRAS'},
 		107: {msg: 'MARTELO -> BATIDA ATRAS'},	
-		108: {msg: 'ATRAS ARREMESO (STUN)'},
-		111: {msg: 'ATRAS GRANDE MARTELO'},
-		//112: {msg: 'Handle! (Perfect block)'},
+		108: {msg: 'ARREMESO ATRAS (STUN)'},
+		111: {msg: 'MARTELO ATRAS '},
+		//112: {msg: 'CABO! (BLOQUEIO PERFEITO)'},
 		113: {msg: 'FALSO (ESQUIVE)'},
 		114: {msg: 'BATIDA FRENTE'},
 		115: {msg: 'BATIDA ATRAS (ENVIO VOANDO)'},//114 115
-		116: {msg: 'Donuts'},
-		117: {msg: 'PULO (FALSO) (BATIDA DERRUBA)'},
-		//118: {msg: 'Jump -> Tank (Knockdown)'},
-		//119: {msg: 'RIGHT safe ->'},
-		//120: {msg: '<- LEFT safe'},
+		116: {msg: 'DONUTS'},
+		117: {msg: 'PULO (FALSO) (BATIDA DERRUBAR)'},
+		//118: {msg: 'PULO -> TANK (DERRUBAR)'},
+		//119: {msg: 'DIREITA SEGURO ->'},
+		//120: {msg: '<- ESQUERDA SEGURO'},
 		121: {msg: 'ONDAS (ESQUERDO)'},
 		122: {msg: 'ONDAS (ESQUERDO) 03 TERCEIRA RAPIDA'},
 		123: {msg: 'ONDAS (ESQUERDO) 02 SEGUNDA RAPIDA'},
 		125: {msg: 'FRENTE, ESQUERDA SEGURO, ATRAS'},//125 126 127
-		131: {msg: 'FRENTE, RIGHT safe, Back'},//131 132 134
-		//135: {msg: 'Handle (Perfect block)'},
-		//127: {msg: 'Back Hammer (chain)'},
-		//134: {msg: 'Back Hammer (chain)'},
+		131: {msg: 'FRENTE, DIREITA SEGURO, ATRAS'},//131 132 134
+		//135: {msg: 'CABO (BLOQUEIO PERFEITO)'},
+		//127: {msg: 'MARTELO ATRAS (CORRENTE)'},
+		//134: {msg: 'MARTELO ATRAS (CORRENTE)'},
 		137: {msg: 'MARTELO ATRAS'},
 		138: {msg: 'ATAQUE PODEROSO MAO ESQUERDA (ENVIO VOANDO)'},//113 138		
-		139: {msg: 'Knockback!!'}, //108 110 139    118 139    101 105 107 139
+		139: {msg: 'BATIDA ATRAS!!'}, //108 110 139    118 139    101 105 107 139
 		140: {msg: 'ONDAS (DIREITA)'},
 		141: {msg: 'ONDAS (DIREITA) 03 TERCEIRA RAPIDA'},
 		142: {msg: 'ONDAS (DIREITA) 02 SEGUNDA RAPIDA'}
-		//305: {msg: 'Meteors!!'},
-		//306: {msg: 'Meteors!!'},
-		//307: {msg: 'Bombs'},//307 301
-		//308: {msg: '1st stun'},
-		//309: {msg: '2nd stun'},
-		//310: {msg: '3rd stun'},
-		//311: {msg: 'Wrath - Right Hand'},//311 120    311 119
-		//312: {msg: 'Wrath - Left Hand'},//312 119    312 120
+		//305: {msg: 'METEOROS!!'},
+		//306: {msg: 'METEOROS!!'},
+		//307: {msg: 'BOMBAS'},//307 301
+		//308: {msg: '01 STUN'},
+		//309: {msg: '02 STUN'},
+		//310: {msg: '03 STUN'},
+		//311: {msg: 'IRA - MAO DIREITA'},//311 120    311 119
+		//312: {msg: 'IRA - MAO ESQUERDA'},//312 119    312 120
 	}
 }
